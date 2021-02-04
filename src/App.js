e3e78baa530e0410afae9396b178ebba45fdd1c3
kai.f <16kbf@queensu.ca>
2021-02-04 14:38:17 -0500

    Added amplify/cognito auth controll for app.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import { BrowserRouter as Router, Switch, useLocation } from "react-router-dom";
+import React from "react";
+import Amplify from "aws-amplify";
+import {
+  AmplifyAuthenticator,
+  AmplifySignUp,
+  AmplifySignOut,
+} from "@aws-amplify/ui-react";
+import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
+import awsconfig from "./aws-exports";
 
-// global styles
-import "./styles/globals.css";
+Amplify.configure(awsconfig);
 
-function App() {
-  return (
-    <Router>
-      <h1>TakeNotes</h1>
-      <p>front page...</p>
-    </Router>
+const AuthStateApp = () => {
+  const [authState, setAuthState] = React.useState();
+  const [user, setUser] = React.useState();
+
+  React.useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
+  }, []);
+
+  return authState === AuthState.SignedIn && user ? (
+    <div className="App">
+      <div>Hello, {user.attributes.email}</div>
+      <AmplifySignOut />
+    </div>
+  ) : (
+    <AmplifyAuthenticator>
+      <AmplifySignUp
+        slot="sign-up"
+        formFields={[
+          { type: "username" },
+          { type: "password" },
+          { type: "email" },
+        ]}
+      />
+    </AmplifyAuthenticator>
   );
-}
+};
 
-export default App;
+export default AuthStateApp;
