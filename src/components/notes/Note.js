4efcd35719abc2af5c138cdbd5dfe31576069cb4
KrishnaSolo <krishna.solanki@mail.utoronto.ca>
2021-04-20 00:23:45 -0400

    got notes view working with text editor with basic editing

--- src/components/notes/Note.js
+++ src/components/notes/Note.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { Button, Paper, Typography, Card } from "@material-ui/core";
 import "../../styles/notes.css";
+import Texteditor from "../TextEditor";
 
 // get content from note
-const content =
+const contentPlaceholder =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed laoreet erat eu diam condimentum imperdiet. Praesent vitae massa sed neque lobortis viverra. Vestibulum euismod mattis suscipit.";
 
 function editNote() {
@@ -15,17 +16,45 @@ function pinNote() {
 }
 
 function Note() {
+  const [edit, setEdit] = useState(false);
+  const [content, setContent] = useState(contentPlaceholder);
+
+  const handleEdit = useCallback(() => {
+    const editState = !edit;
+    setEdit(editState);
+  }, [edit, setEdit]);
+
+  const handleText = useCallback(
+    (text) => {
+      setContent(text);
+    },
+    [setContent]
+  );
+
   return (
     <div className="Note">
-     <Card raised="true">
-         <Paper>
+      <Card raised="true">
+        <Paper>
           <div className="cardContainer">
-            <Typography variant="body2"> {content} </Typography>
-            <Button variant="outlined" size = "small"> edit </Button>
-            <Button variant="outlined" size = "small"> pin </Button>
-            <Button variant="outlined" size = "small"> delete </Button>
-            </div>
-         </Paper>
+            {edit ? (
+              <Texteditor entry={{ content: content }} onChange={handleText} />
+            ) : (
+              //<Typography variant="body2"> {content} </Typography>
+              <div dangerouslySetInnerHTML={{ __html: content }} />
+            )}
+            <Button variant="outlined" size="small" onClick={handleEdit}>
+              {edit ? "save" : "edit"}
+            </Button>
+            <Button variant="outlined" size="small">
+              {" "}
+              pin{" "}
+            </Button>
+            <Button variant="outlined" size="small">
+              {" "}
+              delete{" "}
+            </Button>
+          </div>
+        </Paper>
       </Card>
     </div>
   );
