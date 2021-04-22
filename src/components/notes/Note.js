888b80de5e3c41d43e6a2591091624beb1858989
KrishnaSolo <krishna.solanki@mail.utoronto.ca>
2021-04-22 11:08:22 -0400

    Added functionality to notes dashboard and notes component using redux actions. Also cleaned up code and added local adding and delete  functionality

--- src/components/notes/Note.js
+++ src/components/notes/Note.js
@@ -1,35 +1,53 @@
 import React, { useState, useCallback } from "react";
 import { Button, Paper, Typography, Card } from "@material-ui/core";
+import axios from "axios";
+
 import "../../styles/notes.css";
 import Texteditor from "../TextEditor";
+import { connect } from "react-redux";
+import { reduceUser } from "../../actions/user";
+import { updateNote } from "../../actions/notes";
+
+import { REACT_APP_STAG_BASE } from "../../setupEnv";
 
 // get content from note
 const contentPlaceholder =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed laoreet erat eu diam condimentum imperdiet. Praesent vitae massa sed neque lobortis viverra. Vestibulum euismod mattis suscipit.";
 
-function editNote() {
-  // open text editor within note
-}
-
-function pinNote() {
-  // pin note to side tab
-}
-
-function Note() {
+function Note({ cognitoUser, noteID, content, updateNote, deleteNoteList }) {
   const [edit, setEdit] = useState(false);
-  const [content, setContent] = useState(contentPlaceholder);
+  const [contentText, setContentText] = useState(content || contentPlaceholder);
+  const [noteId, setNoteId] = useState(noteID);
 
-  const handleEdit = useCallback(() => {
+  const saveUpdateNote = useCallback(async () => {
+    console.log("update: ", noteId, "contentText: ", contentText);
+    await updateNote(cognitoUser, noteId, contentText);
+  }, [contentText, noteId]);
+
+  const handleEdit = useCallback(async () => {
+    if (edit) await saveUpdateNote();
     const editState = !edit;
     setEdit(editState);
-  }, [edit, setEdit]);
+  }, [edit, setEdit, saveUpdateNote]);
 
   const handleText = useCallback(
     (text) => {
-      setContent(text);
+      setContentText(text);
     },
-    [setContent]
+    [setContentText]
   );
+
+  const deleteNote = async () => {
+    deleteNoteList(noteID);
+    try {
+      const res = await axios.delete(
+        REACT_APP_STAG_BASE + `users/${cognitoUser.cognitoId}/notes/${noteID}`
+      );
+      reduceUser(res.data.Attributes);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <div className="Note">
@@ -37,21 +55,22 @@ function Note() {
         <Paper>
           <div className="cardContainer">
             {edit ? (
-              <Texteditor entry={{ content: content }} onChange={handleText} />
+              <Texteditor
+                entry={{ content: contentText }}
+                onChange={handleText}
+              />
             ) : (
               //<Typography variant="body2"> {content} </Typography>
-              <div dangerouslySetInnerHTML={{ __html: content }} />
+              <div dangerouslySetInnerHTML={{ __html: contentText }} />
             )}
             <Button variant="outlined" size="small" onClick={handleEdit}>
               {edit ? "save" : "edit"}
             </Button>
             <Button variant="outlined" size="small">
-              {" "}
-              pin{" "}
+              pin
             </Button>
-            <Button variant="outlined" size="small">
-              {" "}
-              delete{" "}
+            <Button variant="outlined" size="small" onClick={deleteNote}>
+              delete
             </Button>
           </div>
         </Paper>
@@ -60,4 +79,8 @@ function Note() {
   );
 }
 
-export default Note;
+const mapStateToProps = (state) => ({
+  cognitoUser: state.userData.cognitoUser,
+});
+
+export default connect(mapStateToProps, { updateNote, reduceUser })(Note);
